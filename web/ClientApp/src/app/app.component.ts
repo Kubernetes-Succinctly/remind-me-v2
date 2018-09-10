d67a95587b875de17e7b64ab5c941a68d3fd9fbe
rahulrai <rahulrai@live.com>
2018-09-10 22:39:40 +1000

    initial commit

---
+++ web/ClientApp/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Reminder } from "./models/reminder";
+import { ReminderDataService } from "./services/reminder-data.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  title: string;
+  dueDate: Date;
+  reminder: Reminder;
+  reminders: Reminder[];
+  minDate: Date = new Date();
+
+  constructor(
+    private fb: FormBuilder,
+    private reminderDataService: ReminderDataService
+  ) {
+    this.form = fb.group({
+      title: [null, Validators.required],
+      dueDate: [null, Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.reminderDataService
+      .getAllReminders()
+      .subscribe(reminders => (this.reminders = reminders));
+  }
+
+  onSubmit(reminder) {
+    this.reminderDataService.addReminder(reminder);
+    this.form.reset();
+  }
+}
---
+++ web/Controllers/ProxyController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+public class ProxyController : Controller
+{
+    public ProxyController()
+    {
+        var client = new RestClient("http://example.com");
+    }
+    public IActionResult SaveReminder(Reminder reminder)
+    {
+        return Ok();
+    }
+    public IActionResult GetAllReminders()
+    {
+        var reminders = new List<Reminder>();
+        reminders.Add(new Reminder { Id = "1", Title = "Test Title", DueDate = DateTime.Today });
+        return Ok(reminders);
+    }
+    public IActionResult DeleteReminder(string reminderId)
+    {
+        return Ok();
+    }
+}
