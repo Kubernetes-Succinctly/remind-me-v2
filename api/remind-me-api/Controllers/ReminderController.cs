1a8b892a8554d3f7ab4fe4690e6c9301e4bda92b
rahulrai <rahulrai@live.com>
2018-12-13 19:06:16 +1100

    removed dead code

--- api/remind-me-api/Controllers/ReminderController.cs
+++ api/remind-me-api/Controllers/ReminderController.cs
@@ -41,11 +41,5 @@ namespace remind_me_api.Controllers
                 remindersCollection.Insert(value);
             }
         }
-
-        [HttpDelete("{id}")]
-        public void Delete(string id)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
